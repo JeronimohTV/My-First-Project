b8c04c2dd4b31701fbe21f3866a84ac80e811451
JeronimohTV <j.marks@live.com>
2024-08-19 14:10:07 +0200

    Completed the user and machine interaction mechanic. Added functions for the checklist item selections and submission. Added rich text box and submit button for user to machine message. Added results section to display interactions.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,19 @@
             checkBox1 = new CheckBox();
             lblSure = new Label();
             checkedListBox1 = new CheckedListBox();
-            button2 = new Button();
+            btnTellMachine = new Button();
+            label2 = new Label();
+            lblTellMachine = new Label();
+            panel1 = new Panel();
+            rtxtUserMessage = new RichTextBox();
+            btnTellMCHN = new Button();
+            panel2 = new Panel();
+            lblUserSays = new Label();
+            lblMachineResults = new Label();
+            btnReset = new Button();
+            label4 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // button1
@@ -48,6 +60,7 @@
             // 
             // label1
             // 
+            label1.ForeColor = Color.Yellow;
             label1.Location = new Point(34, 23);
             label1.Name = "label1";
             label1.Size = new Size(164, 20);
@@ -56,6 +69,7 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
+            checkBox1.ForeColor = Color.DarkOrange;
             checkBox1.Location = new Point(212, 77);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(98, 19);
@@ -67,9 +81,10 @@
             // 
             // lblSure
             // 
+            lblSure.ForeColor = Color.Yellow;
             lblSure.Location = new Point(316, 78);
             lblSure.Name = "lblSure";
-            lblSure.Size = new Size(164, 20);
+            lblSure.Size = new Size(99, 20);
             lblSure.TabIndex = 3;
             lblSure.Text = "Fuck You Again";
             lblSure.Visible = false;
@@ -84,15 +99,123 @@
             checkedListBox1.TabIndex = 4;
             checkedListBox1.Visible = false;
             // 
-            // button2
+            // btnTellMachine
             // 
-            button2.Location = new Point(421, 140);
-            button2.Name = "button2";
-            button2.Size = new Size(138, 34);
-            button2.TabIndex = 5;
-            button2.Text = "Tell the machine";
-            button2.UseVisualStyleBackColor = true;
-            button2.Visible = false;
+            btnTellMachine.Location = new Point(421, 140);
+            btnTellMachine.Name = "btnTellMachine";
+            btnTellMachine.Size = new Size(138, 34);
+            btnTellMachine.TabIndex = 5;
+            btnTellMachine.Text = "Tell the machine";
+            btnTellMachine.UseVisualStyleBackColor = true;
+            btnTellMachine.Visible = false;
+            btnTellMachine.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.ForeColor = Color.Yellow;
+            label2.Location = new Point(575, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(213, 96);
+            label2.TabIndex = 6;
+            label2.Text = "lbl";
+            label2.Visible = false;
+            // 
+            // lblTellMachine
+            // 
+            lblTellMachine.AutoSize = true;
+            lblTellMachine.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblTellMachine.ForeColor = Color.DarkOrange;
+            lblTellMachine.Location = new Point(13, 9);
+            lblTellMachine.Name = "lblTellMachine";
+            lblTellMachine.Size = new Size(203, 15);
+            lblTellMachine.TabIndex = 7;
+            lblTellMachine.Text = "Tell the Machine in your own words";
+            lblTellMachine.Visible = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(rtxtUserMessage);
+            panel1.Controls.Add(btnTellMCHN);
+            panel1.Controls.Add(lblTellMachine);
+            panel1.Location = new Point(34, 187);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(382, 140);
+            panel1.TabIndex = 8;
+            // 
+            // rtxtUserMessage
+            // 
+            rtxtUserMessage.Location = new Point(13, 30);
+            rtxtUserMessage.Name = "rtxtUserMessage";
+            rtxtUserMessage.Size = new Size(363, 66);
+            rtxtUserMessage.TabIndex = 10;
+            rtxtUserMessage.Text = "";
+            rtxtUserMessage.Visible = false;
+            // 
+            // btnTellMCHN
+            // 
+            btnTellMCHN.Location = new Point(238, 99);
+            btnTellMCHN.Name = "btnTellMCHN";
+            btnTellMCHN.Size = new Size(138, 34);
+            btnTellMCHN.TabIndex = 9;
+            btnTellMCHN.Text = "Say your piece";
+            btnTellMCHN.UseVisualStyleBackColor = true;
+            btnTellMCHN.Visible = false;
+            btnTellMCHN.Click += button3_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(lblUserSays);
+            panel2.Controls.Add(lblMachineResults);
+            panel2.Controls.Add(btnReset);
+            panel2.Controls.Add(label4);
+            panel2.Location = new Point(422, 187);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(347, 232);
+            panel2.TabIndex = 10;
+            panel2.Visible = false;
+            // 
+            // lblUserSays
+            // 
+            lblUserSays.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblUserSays.ForeColor = Color.Silver;
+            lblUserSays.Location = new Point(13, 30);
+            lblUserSays.Name = "lblUserSays";
+            lblUserSays.Size = new Size(169, 158);
+            lblUserSays.TabIndex = 12;
+            lblUserSays.Text = "lbl";
+            // 
+            // lblMachineResults
+            // 
+            lblMachineResults.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblMachineResults.ForeColor = Color.Yellow;
+            lblMachineResults.Location = new Point(198, 30);
+            lblMachineResults.Name = "lblMachineResults";
+            lblMachineResults.Size = new Size(138, 156);
+            lblMachineResults.TabIndex = 11;
+            lblMachineResults.Text = "lbl";
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(198, 189);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(138, 34);
+            btnReset.TabIndex = 9;
+            btnReset.Text = "Play again";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label4.ForeColor = Color.DarkOrange;
+            label4.Location = new Point(13, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Results:";
+            label4.Visible = false;
             // 
             // Form1
             // 
@@ -100,7 +223,10 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.HotTrack;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(btnTellMachine);
             Controls.Add(checkedListBox1);
             Controls.Add(lblSure);
             Controls.Add(checkBox1);
@@ -109,6 +235,10 @@
             ForeColor = SystemColors.MenuText;
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -120,6 +250,16 @@
         private CheckBox checkBox1;
         private Label lblSure;
         private CheckedListBox checkedListBox1;
-        private Button button2;
+        private Button btnTellMachine;
+        private Label label2;
+        private Label lblTellMachine;
+        private Panel panel1;
+        private Button btnTellMCHN;
+        private Panel panel2;
+        private Button btnReset;
+        private Label label4;
+        private RichTextBox rtxtUserMessage;
+        private Label lblUserSays;
+        private Label lblMachineResults;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -18,11 +18,65 @@ namespace Test
             if (checkBox1.Checked)
             {
                 lblSure.Visible = true;
+                checkedListBox1.Visible = true;
+                btnTellMachine.Visible = true;
             }
             else
             {
                 lblSure.Visible = false;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string chkmsg = "";
+            label2.Visible = true;
+
+            if (checkedListBox1.CheckedItems.Count > 0)
+            {
+                foreach (string item in checkedListBox1.CheckedItems)
+                {
+                    chkmsg += item + " ";
+                }
+
+                
+                label2.Text = "\"" + chkmsg + "\"" + "\nThat's what you sound like";
+                panel1.Visible = true;
+                lblTellMachine.Visible = true;
+                rtxtUserMessage.Visible = true;
+                btnTellMCHN.Visible = true;
+            }
+            else
+            {
+                label2.Text = "Oh... Too Scared to say something?";
+                panel1.Visible = false;
+                lblTellMachine.Visible = false;
+                rtxtUserMessage.Visible = false;
+                btnTellMCHN.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (lblUserSays.Text != null)
+            {
+                panel2.Visible = true;
+                lblUserSays.Text = rtxtUserMessage.Text;
+                lblMachineResults.Text = "You know what? I respect that. \n I apologise for my cold words. \n Your meat suit deserves more warmth from me.";
+            }
+            else
+            {
+                panel2.Visible = true;
+                lblUserSays.Text = "I'm afraid to say it.";
+                lblMachineResults.Text = "I KNEW YOU WERE TOO COWARDLY TO SPEAK YOUR TRUTH! \n SAY SOMETHING I DARE YOU!";
+                btnReset.Visible = false;
+
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            InitializeComponent();
         }
     }
 }
